15525a53f82f29c73d89e67f4a7c1b4f75345bbf
Gabriel <54512419+gabrieliusbagocius@users.noreply.github.com>
2020-01-11 20:30:46 +0200

    C#

---
+++ Laboratory3/Laboratory1/Manager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Laboratory3
+{
+    public class Manager
+    {
+        private static Random r = new Random();
+
+        public Random random
+        {
+            get { return r; }
+            set { r = value; }
+        }
+        public static void Main()
+        {
+            var nn = new NeuralNetwork();
+
+            double[,] trainingData = new double[20, 2];
+            double inputs;
+            double targets;
+
+            for (var i = 0; i < trainingData.GetLength(0); i++)
+            {
+                trainingData[i, 0] = r.NextDouble();
+                trainingData[i, 1] = (1 + 0.6 * Math.Sin(2 * Math.PI * trainingData[i, 0] / 0.7) + 0.3 * Math.Sin(2 * Math.PI * trainingData[i, 0])) / 2;
+            }
+
+            for (var n = 0; n < 1000; n++)
+            {
+                int rand = r.Next(0, trainingData.GetLength(0));
+                inputs = trainingData[rand, 0];
+                targets = trainingData[rand, 1];
+                nn.Train(inputs, targets);
+            }
+
+            var inputVariables = new double[] { r.NextDouble(), r.NextDouble(), r.NextDouble(), r.NextDouble() };
+            var received = nn.FeedForward(inputVariables[0]);
+            Console.WriteLine("Received: " + received);
+            Console.WriteLine("Actuals: " + (1 + (0.6 * Math.Sin(2 * Math.PI * inputVariables[0] / 0.7)) + (0.3 * Math.Sin(2 * Math.PI * inputVariables[0]))) / 2);
+            Console.WriteLine();
+
+            received = nn.FeedForward(inputVariables[1]);
+            Console.WriteLine("Received: " + received);
+            Console.WriteLine("Actuals: " + (1 + (0.6 * Math.Sin(2 * Math.PI * inputVariables[1] / 0.7)) + (0.3 * Math.Sin(2 * Math.PI * inputVariables[1]))) / 2);
+            Console.WriteLine();
+
+            received = nn.FeedForward(inputVariables[2]);
+            Console.WriteLine("Received: " + received);
+            Console.WriteLine("Actuals: " + (1 + (0.6 * Math.Sin(2 * Math.PI * inputVariables[2] / 0.7)) + (0.3 * Math.Sin(2 * Math.PI * inputVariables[2]))) / 2);
+            Console.WriteLine();
+
+            received = nn.FeedForward(inputVariables[3]);
+            Console.WriteLine("Received: " + received);
+            Console.WriteLine("Actuals: " + (1 + (0.6 * Math.Sin(2 * Math.PI * inputVariables[3] / 0.7)) + (0.3 * Math.Sin(2 * Math.PI * inputVariables[3]))) / 2);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Laboratory3/Laboratory1/NeuralNetwork.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Laboratory3
+{
+    public class NeuralNetwork
+    {
+        private double[] weight = new double[3];
+        private int bias;
+        private double learningRate;
+        private double r1;
+        private double r2;
+        private double c1;
+        private double c2;
+
+        public NeuralNetwork()
+        {
+            Manager manager = new Manager();
+            Random r = manager.random;
+            weight[0] = r.NextDouble();
+            weight[1] = r.NextDouble();
+            weight[2] = r.NextDouble();
+            r1 = r.NextDouble();
+            r2 = r.NextDouble();
+            c1 = r.NextDouble();
+            c2= r.NextDouble();
+            bias = 1;
+            learningRate = 0.1;
+        }
+
+        public double Train(double input, double output)
+        {
+            double predicted = CalculateOutput(input, weight, r1, r2, c1, c2);
+            double localError = Math.Abs(output - predicted);
+
+            if (Math.Abs(localError) > 0.01)
+            {
+                if (localError > 1) localError *= -1;
+                weight[1] = Math.Abs(weight[1] + learningRate * localError * input);
+                weight[2] = Math.Abs(weight[2] + learningRate * localError * input);
+                weight[0] = Math.Abs(weight[0] + learningRate * localError * bias);
+            }
+            return localError;
+
+        }
+        public double FeedForward(double input)
+        {
+            return CalculateOutput(input, weight, r1, r2, c1, c2);
+        }
+
+        private static double CalculateOutput(double input, double[] weight, double r1, double r2, double c1, double c2)
+        {
+            double sumFirstInput = Math.Exp(((input - c1) * (input - c1)) / (2 * r1 * r1)) * weight[1];
+            double sumSecondInput = Math.Exp(((input - c2) * (input - c2)) / (2 * r2 * r2)) * weight[2];
+            double sumAdditional = 1 * weight[0];
+            double sum = sumFirstInput + sumSecondInput + sumAdditional;
+            return sum;
+        }
+
+    }
+}
